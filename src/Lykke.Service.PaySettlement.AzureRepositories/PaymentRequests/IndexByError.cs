99e73e4d2953bb0dab312aa2b72c236873fd61d6
Dmitry Sergeevich Tarasenko <dmitry.tarasenko@lykke.com>
2018-08-30 00:24:46 +0300

    Add error index LPDEV-765

---
+++ src/Lykke.Service.PaySettlement.AzureRepositories/PaymentRequests/IndexByError.cs
@@ -0,0 +1,41 @@
+﻿using AzureStorage.Tables.Templates.Index;
+using System;
+using Lykke.Service.PaySettlement.Core.Domain;
+
+namespace Lykke.Service.PaySettlement.AzureRepositories.PaymentRequests
+{
+    internal static class IndexByError
+    {
+        internal static string GeneratePartitionKey()
+        {
+            return "error";
+        }
+
+        internal static string GenerateRowKey(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            return id;
+        }
+
+        internal static bool IsIndexable(IPaymentRequest entity)
+        {
+            return entity.Error;
+        }
+
+        internal static AzureIndex Create(PaymentRequestEntity entity)
+        {
+            return AzureIndex.Create(GeneratePartitionKey(),
+                GenerateRowKey(entity.Id), entity);
+        }
+
+        internal static AzureIndex Create(string id)
+        {
+            return AzureIndex.Create(GeneratePartitionKey(), GenerateRowKey(id),
+                PaymentRequestEntity.GetPartitionKey(), PaymentRequestEntity.GetRowKey(id));
+        }
+    }
+}
