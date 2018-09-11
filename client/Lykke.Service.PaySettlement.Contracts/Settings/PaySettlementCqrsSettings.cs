5dd627a59ad441454d98c6fa4d5055913604f60e
Dmitry Sergeevich Tarasenko <dmitry.tarasenko@lykke.com>
2018-09-11 15:31:16 +0300

    Add settings to contracts LPDEV-765

---
+++ client/Lykke.Service.PaySettlement.Contracts/Settings/PaySettlementCqrsSettings.cs
@@ -0,0 +1,25 @@
+﻿using Lykke.SettingsReader.Attributes;
+using System.Data;
+
+namespace Lykke.Service.PaySettlement.Contracts.Settings
+{
+    public class PaySettlementCqrsSettings
+    {
+        [AmqpCheck]
+        public string ConnectionString { get; set; }
+
+        public string Environment { get; set; }
+
+        public string Messaging { get; set; }
+
+        public SerializationFormat SerializationFormat { get; set; }
+
+        public string SettlementBoundedContext { get; set; }
+
+        [Optional]
+        public string EventsRoute { get; set; }
+
+        [Optional]
+        public string CommandsRoute { get; set; }
+    }
+}
